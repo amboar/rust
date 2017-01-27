103aa8752f0a83f53d0be4ed84835f37103a242a
Andrew Jeffery <andrew@aj.id.au>
2017-01-27 17:21:57 +1030

    travis: ppc64le: Shift bits off to script, dump network info
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

---
+++ src/ci/run-ppc64le.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cat /etc/os-release
+ldd --version
+whoami
+cat /etc/hosts
+hostname
+ip addr show
+exit 0
+sudo apt-get update
+echo 'deb http://ftp.unicamp.br/pub/ppc64el/ubuntu/14_04/docker-1.6.2-ppc64el/ trusty main' | sudo tee /etc/apt/sources.list.d/docker.list;
+sudo apt-get update
+sudo apt-get install --force-yes docker.io
+mkdir ${HOME}/docker
+src/ci/docker/run.sh $IMAGE;
