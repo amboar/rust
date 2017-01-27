204958f567bcc4c33e01789c57bcbe207a8520d0
Andrew Jeffery <andrew@aj.id.au>
2017-01-27 17:37:16 +1030

    travis: ppc64le: Can we run sudo under docker in docker on tophat?
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>

--- src/ci/run-ppc64le.sh
+++ src/ci/run-ppc64le.sh
@@ -5,11 +5,10 @@ whoami
 cat /etc/hosts
 hostname
 ip addr show
-sudo echo foo
-exit 0
-sudo apt-get update
 echo 'deb http://ftp.unicamp.br/pub/ppc64el/ubuntu/14_04/docker-1.6.2-ppc64el/ trusty main' | sudo tee /etc/apt/sources.list.d/docker.list;
 sudo apt-get update
 sudo apt-get install --force-yes docker.io
+docker run ppc64le/ubuntu:16.04 sudo echo bar
+exit 0
 mkdir ${HOME}/docker
 src/ci/docker/run.sh $IMAGE;
